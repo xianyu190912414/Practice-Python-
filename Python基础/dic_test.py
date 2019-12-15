08e14bdb1298c0130cb31e35d44468cbbaee76b6
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2019-12-15 20:53:13 +0800

    Create dic_test.py

---
+++ Python基础/dic_test.py
@@ -0,0 +1,9 @@
+d = {
+    'Micheal': 95,
+    'Bob': 75,
+    'Tracy': 85
+}
+print('d[\'Micheal\']=', d['Micheal'])
+print('d[\'Bob\']=', d['Bob'])
+print('d[\'Tracy\']=', d['Tracy'])
+print('d.get(\'Thomas\',-1)=', d.get('Thomas'))
