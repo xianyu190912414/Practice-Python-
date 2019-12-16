9bee0700559490b572e44d6dd92268c7798cbfd1
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2019-12-16 08:59:33 +0800

    Delete qua_test.py

--- qua_test.py
+++
@@ -1,9 +0,0 @@
-# 定义一个quadratic(a,b,c)函数，返回ax*x+bx+c=0的两个解
-import math
-def quadratic(a, b, c):
-    k = math.sqrt(b**2-4*a*c)
-    m = (-b+k)/(2*a)
-    n = (-b-k)/(2*a)
-    return m, n
-
-print(quadratic(4, 2, -42))
