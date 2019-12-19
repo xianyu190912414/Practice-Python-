a7f8eae0cfb0eaa18b0cfb97e427a155b9fb8da0
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2019-12-19 19:23:58 +0800

    Create attris.py

---
+++ 面向对象编程/attris.py
@@ -0,0 +1,13 @@
+obj = MyObject()
+
+print('hasattr(obj, \'x\') = ', hasattr(obj, 'x'))
+print('hasattr(obj, \'y\') = ', hasattr(obj, 'y'))
+setattr(obj, 'y', 19)
+print('hasattr(obj, \'y\') = ', hasattr(obj, 'y'))
+print('getattr(obj, \'y\') = ', getattr(obj, 'y'))
+
+print('getattr(obj, \'z\') = ', getattr(obj, 'z', 404))
+
+f = getattr(obj, 'power')
+print(f)
+print(f())
