0cbacb805f8dd5ba45b8f7abcb6fbc326e365b08
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2019-12-19 18:58:15 +0800

    Create type.py

---
+++ 面向对象编程/type.py
@@ -0,0 +1,7 @@
+print('type(123) = ', type(123))
+print('type(\'123\') = ', type('123'))
+# type('type(None) = ', type(None))
+print('type(abs) = ', type(abs))
+
+import types
+print('type(\'abc\') == str? ', type('abc') == str)
