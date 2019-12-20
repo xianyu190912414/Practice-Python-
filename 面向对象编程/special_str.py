b4ab5f17f2060227369fdac80695169932c18545
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2019-12-20 17:47:55 +0800

    Create special_str.py

---
+++ 面向对象编程/special_str.py
@@ -0,0 +1,10 @@
+class Student(object):
+    def __init__(self, name):
+        self.__name = name
+
+    def __str__(self):
+        return 'Student object(name: %s)' % self.__name
+
+    __repr__ = __str__
+
+print(Student('Micheal'))
