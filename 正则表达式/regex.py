4ceafa626a9a3451219e9e9c3f721b6073691d1a
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2019-12-29 12:11:25 +0800

    Update regex.py

--- 正则表达式/regex.py
+++ 正则表达式/regex.py
@@ -2,7 +2,7 @@ import re
 
 print('Test: 010-12345')
 m = re.match(r'^(\d{3})-(\d{3, 8})$', '010-12345')
-# print(m.group(1), m.group(2))
+print(m.group(1), m.group(2))
 
 t = '19:05:30'
 print('Test:', t)
