6da890577870eb959c177fc0f87340a9e233fd3c
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-01-03 11:59:20 +0800

    Create rect.py

---
+++ 图形界面/rect.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+# 设置笔刷宽度
+width(4)
+
+# 前进
+forward(200)
+# 右转90度
+right(90)
+
+# 笔刷颜色
+pencolor('red')
+forward(200)
+right(90)
+
+pencolor("green")
+forward(200)
+right(90)
+
+pencolor('blue')
+forward(200)
+right(90)
+
+done()
